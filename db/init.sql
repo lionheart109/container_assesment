ba61aad36a2cbf593508cde057294bcd0b8546fa
Brajesh Raaghav <61453591+lionheart109@users.noreply.github.com>
2023-05-02 05:15:35 +0530

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -2,9 +2,8 @@ use studentdb;
 CREATE TABLE students (
   id INT AUTO_INCREMENT PRIMARY KEY,
   registration_number VARCHAR(10) NOT NULL,
-  st_name VARCHAR(15),
   vaccinated TINYINT NOT NULL
 );
 
-INSERT INTO students (registration_number,st_name, vaccinated) VALUES ('1234',brajesh, 1);
-INSERT INTO students (registration_number,st_name, vaccinated) VALUES ('5678',raaghav, 1);
+INSERT INTO students (registration_number,st_name, vaccinated) VALUES ('1234', 1);
+INSERT INTO students (registration_number,st_name, vaccinated) VALUES ('5678', 1);
